b65e2f1e92e5941e98f409cb4891a84e66574b37
dogtownoak <david.brandon.ford@gmail.com>
2018-12-04 10:20:30 -0800

    expanded seed files and fixed date fields

--- seed.js
+++ seed.js
@@ -4,13 +4,46 @@ var appetizer_list = [
     {
         type: "Chicken Wings",
         vegan: false,
-        style: "Buffalo",
+        style: "American",
         image: "https://loremflickr.com/320/240/appetizer",
         desription: "Bone-in Buffalo style hot wings",
-        dateValid: Date(2018/12/12),
+        dateValid: new Date("12-13-2018"),
         business: "Original Buffalo Wings",
         businessAddress: "2499 Lombard Street, San Francisco CA 94123",
         businessLocation: "{lat: 37.798864, lng: -122.4424}"
+    },
+    {
+        type: "Poke Nachos",
+        vegan: false,
+        style: "Buffalo",
+        image: "https://loremflickr.com/320/240/appetizer",
+        desription: "Bone-in Buffalo style hot wings",
+        dateValid: new Date("12-13-2018"),
+        business: "Original Buffalo Wings",
+        businessAddress: "2499 Lombard Street, San Francisco CA 94123",
+        businessLocation: "{lat: 37.798864, lng: -122.4424}"
+    },
+    {
+        type: "Sliders",
+        vegan: false,
+        style: "American",
+        image: "https://loremflickr.com/320/240/appetizer",
+        desription: "Filet mignon sliders",
+        dateValid: new Date("12-13-2018"),
+        business: "Kincaid's Oakland",
+        businessAddress: "1 Frannklin St, Oakland, CA 94607",
+        businessLocation: "{lat: 37.793228, lng: -122.392883}"
+    },
+    {
+        type: "Hummus Platter",
+        vegan: true,
+        style: "Middle Eastern",
+        image: "https://loremflickr.com/320/240/appetizer",
+        desription: "Hummus platter with pita chips",
+        dateValid: new Date("12-13-2018"),
+        business: "Arabian Nights Restaurant",
+        businessAddress: "2345 Mission St, San Francisco, CA 94110",
+        businessLocation: "{lat: 37.748470, lng: -122.418110}"
     }
 ];
 
@@ -22,7 +55,43 @@ var drink_list = [
         brand: "Anderson Valley Brewing Company",
         image: "https://loremflickr.com/320/240/beer",
         desription: "5.8% ALC, Bitterness: 16 IBU",
-        dateValid: 13/12/2018,
+        dateValid: new Date("12-13-2018"),
+        business: "Rickhouse",
+        businessAddress: "246 Kearny Street, San Francisco CA 94108",
+        businessLocation: "{lat: 37.790499, lng: -122.403839}"
+    },
+    {
+        type: "Beer",
+        vegan: true,
+        style: "Boont Amber Ale",
+        brand: "Anderson Valley Brewing Company",
+        image: "https://loremflickr.com/320/240/beer",
+        desription: "5.8% ALC, Bitterness: 16 IBU",
+        dateValid: new Date("12-13-2018"),
+        business: "Rickhouse",
+        businessAddress: "246 Kearny Street, San Francisco CA 94108",
+        businessLocation: "{lat: 37.790499, lng: -122.403839}"
+    },
+    {
+        type: "Beer",
+        vegan: true,
+        style: "Boont Amber Ale",
+        brand: "Anderson Valley Brewing Company",
+        image: "https://loremflickr.com/320/240/beer",
+        desription: "5.8% ALC, Bitterness: 16 IBU",
+        dateValid: new Date("12-13-2018"),
+        business: "Rickhouse",
+        businessAddress: "246 Kearny Street, San Francisco CA 94108",
+        businessLocation: "{lat: 37.790499, lng: -122.403839}"
+    },
+    {
+        type: "Beer",
+        vegan: true,
+        style: "Boont Amber Ale",
+        brand: "Anderson Valley Brewing Company",
+        image: "https://loremflickr.com/320/240/beer",
+        desription: "5.8% ALC, Bitterness: 16 IBU",
+        dateValid: new Date("12-13-2018"),
         business: "Rickhouse",
         businessAddress: "246 Kearny Street, San Francisco CA 94108",
         businessLocation: "{lat: 37.790499, lng: -122.403839}"
@@ -30,6 +99,36 @@ var drink_list = [
 ];
 
 var user_list = [
+    {
+        firstName: "Nancy",
+        lastName: "Johnson",
+        email: "nancyjohnson@gmail.com",
+        phone: "415-123-4567",
+        userName: "nancyjohnson",
+        password: "password1",
+        image: "https://loremflickr.com/320/240/headshot",
+        drinkPlanType: "Beer Plan",
+        drinkPerWeek: 2,
+        drinkUsed: 0,
+        appetizerPlanType: "Appetizer Plan",
+        appetizerPerWeek: 3,
+        appetizerUsed: 0
+    },
+    {
+        firstName: "Nancy",
+        lastName: "Johnson",
+        email: "nancyjohnson@gmail.com",
+        phone: "415-123-4567",
+        userName: "nancyjohnson",
+        password: "password1",
+        image: "https://loremflickr.com/320/240/headshot",
+        drinkPlanType: "Beer Plan",
+        drinkPerWeek: 2,
+        drinkUsed: 0,
+        appetizerPlanType: "Appetizer Plan",
+        appetizerPerWeek: 3,
+        appetizerUsed: 0
+    },
     {
         firstName: "Nancy",
         lastName: "Johnson",
