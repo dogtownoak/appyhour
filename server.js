4306b198174fa10bf1d6a69b18c34592b6e795fc
dogtownoak <david.brandon.ford@gmail.com>
2018-12-06 12:18:44 -0800

    oved appetizers to new view.  Deleted index/lp/drinks to avoid conflicts from -R

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/appetizers', (req, res) => {
+    res.sendFile(__dirname + '/views/appetizers.html');
+})
+
 // app.get('/signup', (req, res) => {
 //     res.sendFile(__dirname + '/views/signup.html');
 // });
